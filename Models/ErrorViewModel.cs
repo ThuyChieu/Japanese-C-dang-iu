5902c66b306a1691600df4a26643712e88f9c598
Nguyễn Lê Anh Minh <anhminh2122000@gmai.com>
2022-03-24 00:33:56 +0700

    Init ASP.NET MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Japanese_C_dang_iu.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
